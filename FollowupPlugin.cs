da243496cf0b45a39f255769d9bdb60eeb44e6dd
anony44 <59768534+anony44979@users.noreply.github.com>
2021-03-17 08:16:30 -0700

    Add files via upload

---
+++ FollowupPlugin.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace PluginClass1
+{ 
+    /// The plug-in creates a task activity after a new account is created. The activity reminds the user to
+    /// follow-up with the new account customer one week after the account was created.
+    /// <remarks>Register this plug-in on the Create message, account entity, and asynchronous mode.
+    /// </remarks>
+
+    public sealed class FollowupPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {   // the platform provides us with the service provider object
+            //Extract the tracing service for use in debugging sandboxed plug-ins.
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.
+            //Using this Inputparamter we can get the entity and other types of dynamic CRM.
+
+            //Target is one of the main parameters of this IPluginExecutionContext object.
+
+            //All the plug -in events are first stored in this variable called “Target”.
+
+            //So when any events occur we need to check if this variable is there in our current context execution.
+
+            //And if so then we need to check if it's an entity. Because for this article we will trigger a plug-in if a new account (Entity) is created.
+            // Target is an object of type Entity. It is a part of input parameters and its key is Target and value is the target object.It is the account entity that u have registered for which plugin fires.
+            //
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Verify that the target entity represents an account.
+                // If not, this plug-in was not registered correctly.
+                if (entity.LogicalName != "account")
+                    return;
+
+                try
+                {
+                    // Create a task activity to follow up with the account customer in 7 days. 
+                    Entity followup = new Entity("task");
+
+                    followup["subject"] = "Send e-mail to the new customer.";
+                    followup["description"] =
+                        "Follow up with the customer. Check if there are any new issues that need resolution.";
+                    followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                    followup["scheduledend"] = DateTime.Now.AddDays(7);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    // Refer to the account in the task activity.
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjectidType = "account";
+
+                        followup["regardingobjectid"] =
+                        new EntityReference(regardingobjectidType, regardingobjectid);
+                    }
+
+                    // Obtain the organization service reference.
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    // Create the task in Microsoft Dynamics CRM.
+                    tracingService.Trace("FollowupPlugin: Successfully created the task activity.");
+                    service.Create(followup);
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in the FollowupPlugin plug-in.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowupPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
